d7723d86e7c375f7d03a13d0069f5cb3f0fe9173
TomekGin <tomek.ginszt@gmail.com>
2022-09-25 23:04:38 +0200

    add

--- src/main/java/com/ginszt/calculator/CalculatorController.java
+++ src/main/java/com/ginszt/calculator/CalculatorController.java
@@ -1,27 +1,48 @@
 package com.ginszt.calculator;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CalculatorController {
 
     @GetMapping("/")
     public String calc() {
-        return "calculator/calc2.html";
+        return "calculator/calc.html";
+    }
+
+    @RequestMapping(path = "/type", method = RequestMethod.POST)
+    public String handleForm(
+            @RequestParam("type") String type, Model model) {
+
+        System.out.println("type: " + type);
+
+        model.addAttribute("type", type);
+
+        return add();
     }
 
     @GetMapping("/add")
     public String add() {
         return "calculator/add.html";
     }
+    @RequestMapping(path = "/processformAdd", method = RequestMethod.POST)
+    public String handleForm(
+            @RequestParam("firstValue") Double firstValue,
+            @RequestParam("secondValue") Double secondValue, Model model) {
 
-    @GetMapping("/add//{firstInput}/{secondInput}")
-    public CalculatorDto add(@PathVariable double firstInput,
-                             @PathVariable double secondInput) {
-        return new CalculatorDto(firstInput, secondInput,
-                (double)Math.round((firstInput + secondInput) * 100) / 100);
+        System.out.println("firstValue: " + firstValue);
+        System.out.println("secondValue: " + secondValue);
+
+        model.addAttribute("firstValue", firstValue);
+        model.addAttribute("secondValue", secondValue);
+
+        double result = (double)Math.round((firstValue + secondValue) * 100) / 100;
+        System.out.println(result);
+        model.addAttribute("result", result);
+
+        return "calculator/result.html";
     }
 
     @GetMapping("/sub/{firstInput}/{secondInput}")
