6034bd08a6923f1141e6a6530575a4a0f48c5462
TomekGin <tomek.ginszt@gmail.com>
2022-09-26 03:16:56 +0200

    calc 1.1

--- src/main/java/com/ginszt/calculator/CalculatorController.java
+++ src/main/java/com/ginszt/calculator/CalculatorController.java
@@ -9,43 +9,63 @@ public class CalculatorController {
 
     @GetMapping("/")
     public String calc() {
-        return "calculator/calc.html";
+//        return "calculator/calc.html";
+        return "calculator/calculator.html";
     }
 
-    @RequestMapping(path = "/type", method = RequestMethod.POST)
-    public String handleForm(@RequestParam("type") String type, Model model) {
-        System.out.println("type: " + type);
+    @RequestMapping(path = "/calculate", method = RequestMethod.POST)
+    public String handleForm(@RequestParam("type") String type,
+                             @RequestParam("firstValue") Double firstValue,
+                             @RequestParam("units1") String units1,
+                             @RequestParam("secondValue") Double secondValue,
+                             @RequestParam("units2") String units2,
+                             @RequestParam("units3") String units3,
+                             Double result,
+                             Model model) {
+
         model.addAttribute("type", type);
-        if (type.equals("+")) {
-            return "calculator/add.html";
-        } else if (type.equals("-")) {
-            return "calculator/substract.html";
-        } else if (type.equals("*")) {
-            return "calculator/multiply.html";
-        } else if (type.equals("/")) {
-            return "calculator/divide.html";
-        } else {
-            System.out.println("Wrong input");
-        }
-        return "";
-    }
-
-    @RequestMapping(path = "/add", method = RequestMethod.POST)
-    public String handleForm(
-            @RequestParam("firstValue") Double firstValue,
-            @RequestParam("secondValue") Double secondValue, Model model) {
-
-        System.out.println("firstValue: " + firstValue);
-        System.out.println("secondValue: " + secondValue);
-
         model.addAttribute("firstValue", firstValue);
         model.addAttribute("secondValue", secondValue);
+        model.addAttribute("units1", units1);
+        model.addAttribute("units2", units2);
+        model.addAttribute("units3", units3);
 
-        double result = (double)Math.round((firstValue + secondValue) * 100) / 100;
-        System.out.println(result);
+        if (units1.equals("ft")) {
+            firstValue = firstValue / 3.2808399;
+        } else if (units1.equals("NM")) {
+            firstValue = firstValue / 0.000539956803;
+        } else {
+            firstValue = firstValue;
+        }
+
+        if (units2.equals("ft")) {
+            secondValue = secondValue / 3.2808399;
+        } else if (units2.equals("NM")) {
+            secondValue = secondValue / 0.000539956803;
+        } else {
+            secondValue = secondValue;
+        }
+
+        if (type.equals("+")) {
+            result = firstValue + secondValue;
+        } else if (type.equals("-")) {
+            result = firstValue - secondValue;
+        } else if (type.equals("*")) {
+            result = firstValue * secondValue;
+        } else if (secondValue != 0){
+            result = firstValue / secondValue;
+        } else return "calculator/warning.html";
+
+        if (units3.equals("ft")) {
+            result = result / 3.2808399;
+        } else if (units3.equals("NM")) {
+            result = result / 0.000539956803;
+        } else {
+            result = result;
+        }
+
+        result = (double) Math.round(result * 100) / 100;
         model.addAttribute("result", result);
-
         return "calculator/result.html";
     }
-
 }
